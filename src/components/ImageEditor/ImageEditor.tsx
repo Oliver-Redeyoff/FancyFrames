320fa49444878a8db987fd45cae6be487872d743
OliverRedeyoff <oliver.redeyoff@gmail.com>
2024-01-10 17:22:14 +0000

    added download button which downloads the image

--- src/components/ImageEditor/ImageEditor.tsx
+++ src/components/ImageEditor/ImageEditor.tsx
@@ -1,22 +1,44 @@
-import React, { useState } from "react";
+import "./ImageEditor.css";
+
+import { useState, useRef } from "react";
+import { toPng } from "html-to-image";
 
 const ImageEditor = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const elementRef = useRef<HTMLDivElement>(null);
+
+  function htmlToImageConvert() {
+    if (elementRef != null) {
+      toPng(elementRef.current as HTMLElement, { cacheBust: false })
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.download = "my-image-name.png";
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
 
   return (
     <div>
       <h1>Upload and Display Image usign React Hook's</h1>
 
       {selectedImage && (
-        <div>
-          <img
-            alt="not found"
-            width={"250px"}
-            src={URL.createObjectURL(selectedImage)}
-          />
-          <br />
+        <>
+          <div ref={elementRef} className="fancy-image">
+            <img
+              alt="not found"
+              width={"250px"}
+              src={URL.createObjectURL(selectedImage)}
+            />
+          </div>
+
           <button onClick={() => setSelectedImage(null)}>Remove</button>
-        </div>
+          <button onClick={() => htmlToImageConvert()}>Download</button>
+        </>
       )}
 
       <br />
