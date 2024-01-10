95163dbec7c173b4188394e6bbbdfc24b3d95b56
OliverRedeyoff <oliver.redeyoff@gmail.com>
2024-01-10 17:08:09 +0000

    added basic webpage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ImageEditor from "./components/ImageEditor/ImageEditor";
+
+function App() {
+  return (
+    <div className="App">
+      <ImageEditor />
+    </div>
+  );
+}
+
+export default App;
